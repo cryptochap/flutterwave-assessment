65b466f8d7552a3be518b26f67f47638a22335a8
samson olusegun <olusegunsamson@live.com>
2021-01-26 02:34:19 +0100

    updated entry file

--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
-const express = require('express')
-const app = express()
-const PORT= process.env.PORT || 3000
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.get('/name', (req,res) =>{
-  res.json({
-    names:["samson","olusegun"],
-    age:12,
+var PORT = process.env.PORT || 3000;        // set our port
 
-  })
-})
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
 
-app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`)})
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(PORT);
+console.log('Server Starting at ' + PORT);
